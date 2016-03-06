ca9916759a3add395285af20f08bce7946dee7e9
volkanto <dev@tokmak.org>
2016-03-07 01:38:52 +0200

    developer table fix

--- src/main/resources/static/app/developer/developer.controller.js
+++ src/main/resources/static/app/developer/developer.controller.js
@@ -1,6 +1,15 @@
 angular.module('issueTracker').controller('DeveloperController', DeveloperController);
 
 function DeveloperController($scope) {
-	$scope.developerList = [];
-	console.log(1);
+	$scope.developerList = [{id: 1, name: 'volkan1'}, {id: 2, name: 'volkan2'}, {id: 3, name: 'volkan3'}];
+
+	$scope.addNew = function() {
+		var newOne = {id: 4, name: 'volkan4'};
+		$scope.developerList.push(newOne);
+	};
+	
+	$scope.remove = function(argIndex) {
+		console.log(argIndex);
+		$scope.developerList.splice(argIndex, 1);
+	};
 }
